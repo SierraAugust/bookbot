765596db9cf109fe87ca822fb20983e8e8125d37
SierraAugust <107669073+SierraAugust@users.noreply.github.com>
2025-08-09 17:13:09 +0000

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-def get_book_text(filepath):
-    with open(filepath) as book:
-        content = book.read()
-    return content
-
-
-
 def main():
-     print(get_book_text("books/frankenstein.txt"))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_book_text(path):
+    with open(path) as book:
+        return book.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
 main()
+
+
+
+
+
+
+    
+    
+
